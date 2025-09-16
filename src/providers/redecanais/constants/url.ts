7e5b76bac157d0c97cbbe819c635a4cb82d3a44b
spywarexxx <s69333625@protonmail.com>
2025-09-16 17:55:03 -0300

    Update url.ts

--- src/providers/redecanais/constants/url.ts
+++ src/providers/redecanais/constants/url.ts
@@ -1,4 +1,4 @@
-export const PROVIDER_URL = 'https://redecanais.gy';
+export const PROVIDER_URL = 'https://redecanais.ee';
 export const MOVIES_URL = '/mapafilmes.html';
 export const SERIES_URL = '/mapa.html';
 export const PLAYER_URL = '/player3';
